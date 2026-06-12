ef6b17774975e7609b886e863a8d0bba6628f26f
swet <swetpatel09032005@gmail.com>
2026-06-12 16:04:37 +0530

    url commit

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "http://localhost:5000",
+  "https://chat-app-hc7g.onrender.com",
   {
     autoConnect: false,
     withCredentials: true,
