583bcdf95dd751f0b0df1757a2a30029e41d9857
swet <swetpatel09032005@gmail.com>
2026-06-12 16:22:12 +0530

    frontend backend both

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-baseURL: "https://chat-app-hc7g.onrender.com",
+baseURL: "https://chat-app-hc7g.onrender.com/api",
 withCredentials: true,
 });
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "https://chat-app-hc7g.onrender.com",
+  "https://chat-app-hc7g.onrender.com/api",
   {
     autoConnect: false,
     withCredentials: true,
