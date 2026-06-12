38551c22fabe02b069edfa75a1210550fbaa47c7
swet <swetpatel09032005@gmail.com>
2026-06-12 16:10:52 +0530

    frontend backend both depoyed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-teal-seven-33.vercel.app",
     credentials: true,
   })
 );
